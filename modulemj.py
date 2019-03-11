487abefbb441c5d067c9ce712685df0f06ffdf44
Manoj Bahadur <47881066+manojbahadur@users.noreply.github.com>
2019-03-11 09:03:39 +0530

    Add files via upload

---
+++ modulemj.py
@@ -0,0 +1,10 @@
+import module
+s=module.sum(4,5)
+m=module.mult(10,40)
+print(s,m)  
+'''
+
+
+from module import sum
+s=sum(3,89)
+print(s)'''
---
+++ moludemj.py
@@ -0,0 +1,3 @@
+import module
+s=module.sum(4,5)
+m=module.mult(10,40)
