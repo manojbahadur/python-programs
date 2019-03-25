94b66d1e9c57fd68d0271d1c5a16adcce96bd876
Manoj Bahadur <47881066+manojbahadur@users.noreply.github.com>
2019-03-25 08:41:42 +0530

    Add files via upload

---
+++ evenoddbt100_5000_mj.py.py
@@ -0,0 +1,6 @@
+#to find even or odd no between 100 and 5000
+for int i in range(100,500):
+	if i%2 == 0:
+		print(i,"the given number is even")
+	else:
+		print(i,"the given number is odd")
---
+++ evenorodd_mj.py
@@ -0,0 +1,6 @@
+#to check the given no is even or not
+x = int(input("Enter a number to find is it even or odd: "))
+if x%2 == 0:
+	print("the given number is even")
+else:
+	print("the given number is odd")
