645e1b1213f751e8f2c32796ef03c49ad7a9fd30
Manoj Bahadur <47881066+manojbahadur@users.noreply.github.com>
2019-03-25 08:41:16 +0530

    Add files via upload

--- sending_mailmj.py
+++ sending_mailmj.py
@@ -1,14 +1,13 @@
+#sending mail
 import smtplib
 try:
-	s=smtplib.SMTP('smtp.gmail.com',587)
-	s.starttls()
-	sender='python.program27@gmail.com'
-	receiver='manoj70195@gmail.com'
-	msg='hello'
-	s.login(sender,"python.program#277")
-	s.sendmail(sender,receiver,msg)
+s=smtplib.SMTP('smtp.gmail.com','587')
+s.starttls()
+sender="manoj70195@gmail.com"
+receiver="prathyushareddy1629@gmail.com"
+msg="Hello"
+s.login(sender,'pass')
+s.sendmail(sender,recevier,msg)
 except:
-	print('some error occured')
-else:
-	print('message sent sucessful')
-	s.quit()
+	print("uncessful")
+s.quit()
