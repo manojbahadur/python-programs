e1e62380d1f8aa5a9c871e7959d90260a0ab9c69
Manoj Bahadur <47881066+manojbahadur@users.noreply.github.com>
2019-03-29 11:11:05 +0530

    Add files via upload

---
+++ pattern_mj.py
@@ -0,0 +1,23 @@
+  # Function to demonstrate printing pattern triangle 
+def triangle(n): 
+    k = 2*n - 2
+    for i in range(0, n): 
+        for j in range(0, k): 
+            print(end=" ") 
+        k = k - 1
+        for j in range(0, i+1): 
+            print("* ", end="") 
+        print("\r") 
+  
+
+n = int(input("Enter the no of rows"))
+triangle(n) 
+
+
+
+
+
+
+
+
+
